46258dbba277da0ab9f3290d52121cf777e445a2
Muratov <muratoff101088@gmail.com>
2022-10-25 20:55:04 +0300

    Д/З №10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        public static void main(String[] args) {
+            task1();
+            task2();
+            task3();
+        }
+        public static void task1() {
+            String firstName = "Ivan";
+            String middleName = "Ivanovich";
+            String lastName = "Ivanov";
+            String fullName = lastName + " " + firstName + " " + middleName;
+            System.out.printf("ФИО сотрудника — %s \n", fullName);
+        }
+        public static void task2() {
+            String fullName = "Ivanov Ivan Ivanovich";
+            String upperFullName = fullName.toUpperCase();
+            System.out.println("Данные ФИО сотрудника для заполнения отчёта - " + upperFullName);
+        }
+
+        public static void task3() {
+            String fullName = "Иванов Семён Семёнович";
+            fullName = fullName.replace("ё", "е");
+            System.out.println("ФИО сотрудника - " + fullName);
+        }
+
+
     }
-}
+
+
